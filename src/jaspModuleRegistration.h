9edd273270599605ae915b359398b421cc8552a1
vandenman <donvdbergh@hotmail.com>
2021-11-26 13:34:45 +0100

    replicate tibble printing a bit

--- src/jaspModuleRegistration.h
+++ src/jaspModuleRegistration.h
@@ -26,6 +26,12 @@ RCPP_MODULE(jaspResults)
 	Rcpp::function("cpp_progressbarTick",	jaspResults::staticProgressbarTick);
 
 	Rcpp::function("destroyAllAllocatedObjects", jaspObject::destroyAllAllocatedObjects);
+
+	Rcpp::function("printDevInfo",		jaspObject::setPrintDevInfo);
+	Rcpp::function("setIndentSize",		jaspObject::setIndentSize);
+	Rcpp::function("useUnicode",		jaspObject::setUseUnicode);
+	Rcpp::function("indentWithTabs",	jaspObject::setIndentWithTabs);
+
 	Rcpp::class_<jaspObject_Interface>("jaspObject")
 
 		.method("print",							&jaspObject_Interface::print,											"Prints the contents of the jaspObject")
